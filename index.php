fad65933238720e939617b0bddcdfba1a8ecf04a
Nels <ncfindley2@gmail.com>
2016-07-05 11:32:23 -0500

    Working on rows

--- index.php
+++ index.php
@@ -6,25 +6,68 @@
 </head>
 <body>
 	<div class = "navbar">
-		<form class = "navbar__search">
-			Search movies ...
-			<input type="search" name="moviesearch">
-			<input type="submit">
-		</form>
 
-		<h1 class = "navbar__main">Main</h1>
-
-		<div class = "main">
-			
-
-
-
+		<div class = "search"> 
+			<div class = "search__1">
+				<span class="search_icon"></span>
+				<input type="search" id = "search_search" placeholder="Search movies">
 
 		</div>
 
+		<h1 class = "navbar__title">Main</h1>
+
+		<ul class = "main">
+
+			<li class = "main__item"><a href = "#">Playing</a></li>
+			<li class = "main__item"><a href = "#">Discover</a></li>
+			<li class = "main__item"><a href = "#">Activity</a></li>
+			<li class = "main__item"><a href = "#">Top Charts</a></li>
+			<li class = "main__item"><a href = "#">New Releases</a></li>
+
+		</ul>
+
+		<h1 class = "navbar__title">Your Movie Circles</h1>
+
+		<ul class = "circles">
+			<li class = "circles__item"><a href = "#">This I Must See</a></li>
+			<li class = "circles__item"><a href = "#">The Best</a></li>
+		</ul>
 
 
+		<h1 class = "navbar__title">Collections</h1>
+
+		<ul class = "collections">
+			
+			<li class = "collections__item"><a href = "#">Me and Bob</a></li>
+			<li class = "collections__item"><a href = "#">Travel Movies</a></li>
+		</ul>
 
 	</div>
+
+	<div class="row">
+
+		<div class="movie">
+		<img class = "movie__image" src="The Wolverine.jpeg">
+		<h2>The Wolverine</h2>
+		<p>Action, Adventure</p>
+		<p>126 min</p>
+		</div>
+
+		<div class="movie">
+		<img class = "movie__image" src="Independence Day.jpeg">
+		<h2>Independence Day</h2>
+		<p>Action</p>
+		<p>120 min</p>
+		</div>
+
+		<div class = "movie">
+		<img class = "movie__image" src="The Wolverine.jpeg">
+		<h2>The Wolverine</h2>
+		<p>Action, Adventure</p>
+		<p>126 min</p>
+		</div>
+
+	</div>
+
 </body>
 </html>
