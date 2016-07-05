688de20bb2fe9922f765bbdcd5911ff936017809
Nels <ncfindley2@gmail.com>
2016-07-05 09:54:43 -0500

    Starting movie_design

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<html>
+<head>
+	<title>Discover</title>
+</head>
+<body>
+	<div class = "navbar">
+		<form class = "navbar__search">
+			Search movies ...
+			<input type="search" name="moviesearch">
+			<input type="submit">
+		</form>
+
+		<h1 class = "navbar__main">Main</h1>
+
+		<div class = "main">
+			
+
+
+
+
+		</div>
+
+
+
+
+	</div>
+</body>
+</html>
