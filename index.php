39e98411394200211d9d8359c4d5bb03af3fa2f4
Nels <ncfindley2@gmail.com>
2016-07-06 12:25:12 -0500

    add/fix flex

--- index.php
+++ index.php
@@ -4,142 +4,169 @@
 <head>
 	<title>Discover</title>
 </head>
-<body>
-	<div class = "navbar">
+<body class="super-container">
+	<div class = "container">
+		<div class = "navbar">
 
-		<div class = "search"> 
-			<div class = "search__1">
-				<span class="search_icon"></span>
-				<input type="search" id = "search__box" placeholder="Search movies">
+			<div class = "search"> 
+				<div class = "search__1">
+					<span class="search_icon"></span>
+					<input type="search" id = "search__box" placeholder="Search movies">
+				</div>
+
 			</div>
 
-		</div>
+			<h1 class = "navbar__title">Main</h1>
 
-		<h1 class = "navbar__title">Main</h1>
+			<ul class = "main">
 
-		<ul class = "main">
+				<li class = "main__play l-main__item"><a class="main__link" href = "#">Playing</a></li>
+				<li class = "main__discover l-main__item"><a class="main__link" href = "#">Discover</a></li>
+				<li class = "main__activity l-main__item"><a class="main__link" href = "#">Activity</a></li>
+				<li class = "main__topChart l-main__item"><a class="main__link" href = "#">Top Charts</a></li>
+				<li class = "main__newRelease l-main__item"><a class="main__link" href = "#">New Releases</a></li>
 
-			<li class = "main__item"><a href = "#">Playing</a></li>
-			<li class = "main__item"><a href = "#">Discover</a></li>
-			<li class = "main__item"><a href = "#">Activity</a></li>
-			<li class = "main__item"><a href = "#">Top Charts</a></li>
-			<li class = "main__item"><a href = "#">New Releases</a></li>
-
-		</ul>
-
-		<h1 class = "navbar__title">Your Movie Circles</h1>
-
-		<ul class = "circles">
-			<li class = "circles__item"><a href = "#">This I Must See</a></li>
-			<li class = "circles__item"><a href = "#">The Best</a></li>
-		</ul>
-
-
-		<h1 class = "navbar__title">Collections</h1>
-
-		<ul class = "collections">
-			
-			<li class = "collections__item"><a href = "#">Me and Bob</a></li>
-			<li class = "collections__item"><a href = "#">Travel Movies</a></li>
-		</ul>
-
-	</div>
-
-<!-- Header   -->
-	<div class = "header">
-		<h1>Discover</h1>
-
-
-
-		<ul class= "header__row">
-			<li>Release Date</li>
-			<li> | </li>
-			<li>Popularity</li>
-
-		</ul>
-
-	</div>
-
-<!-- Rows of Movies   -->
-	<div class="row">
-
-		<div class="movie">
-			<img class = "movie__image" src="The_Wolverine.jpeg">
-			<h2 class = "movie__title">The Wolverine</h2>
-			<p class = "move__genre">Action, Adventure</p>
-			<p class = "movie__time">126 min</p>
-		</div>
-
-		<div class="movie">
-			<img class = "movie__image" src="Independence_Day.jpeg">
-			<h2 class = "movie__title">Independence Day</h2>
-			<p class = "move__genre">Action</p>
-			<p class = "movie__time">120 min</p>
-		</div>
-
-		<div class = "movie">
-			<img class = "movie__image" src="The_Wolverine.jpeg">
-			<h2 class = "movie__title">The Wolverine</h2>
-			<p class = "move__genre">Action, Adventure</p>
-			<p class = "movie__time">126 min</p>
-		</div>
-
-		<div class="movie">
-			<img class = "movie__image" src="Independence_Day.jpeg">
-			<h2 class = "movie__title">Independence Day</h2>
-			<p class = "move__genre">Action</p>
-			<p class = "movie__time">120 min</p>
-		</div>
-
-		<div class="movie">
-			<img class = "movie__image" src="Independence_Day.jpeg">
-			<h2 class = "movie__title">Independence Day</h2>
-			<p class = "move__genre">Action</p>
-			<p class = "movie__time">120 min</p>
-		</div>
-	</div>
-
-<!-- Start of chat bar  -->
-
-	<div class="chat">
-
-		<div class = "chat__user">
-			<ul>
-				<li>Nels Findley</li>
-			</ul>
-		</div>
-
-		<div class = "chat__online">
-
-			<h2 class = "chat__title">Online</h2>
-			<ul>
-				<li>Victor Erixon</li>
-					<p class = "chat__watching">The Great Gatsby</p>
-				<li>Gustav Butlex</li>
-					<p class = "chat__watching">Wolverine</p>
 			</ul>
 
-			<h2 class = "chat__title">Offline</h2>
+			<h1 class = "navbar__title">Your Movie Circles</h1>
 
-			<ul>
-				<li>Bill S Kenney</li>
-					<p class = "chat__watching">Jobs</p>
-				<li>Charlie Waite</li>
-					<p class = "chat__watching">Pacific Rim</p>
+			<ul class = "circles">
+				<li class = "circles__item"><a class="main__link" href = "#">This I Must See</a></li>
+				<li class = "circles__item"><a class="main__link" href = "#">The Best</a></li>
+			</ul>
+
+
+			<h1 class = "navbar__title">Collections</h1>
+
+			<ul class = "collections">
+				
+				<li class = "collections__item"><a class="main__link" href = "#">Me and Bob</a></li>
+				<li class = "collections__item"><a class="main__link" href = "#">Travel Movies</a></li>
 			</ul>
 
 		</div>
-		
+
+		<div class = "middle-content">
+
+		<!-- Header   -->
+			<div class = "header">
+				<h1>Discover</h1>
 
 
 
+				<ul class= "sortLinks">
+					<li class="sortLinks__release">Release Date</li>
+					<li class="sortLinks__divider"> | </li>
+					<li class="sortLinks_popularity">Popularity</li>
 
+				</ul>
 
+			</div>
 
+	<!-- Rows of Movies   -->
+			<div class="movie-content">
 
+				<div class="movie l-movie">
+					<img class = "movie__image" src="The_Wolverine.jpeg">
+					<h2 class = "movie__title">The Wolverine</h2>
+					<p class = "move__genre">Action, Adventure</p>
+					<p class = "movie__time">126 min</p>
+				</div>
 
+				<div class="movie l-movie">
+					<img class = "movie__image" src="Independence_Day.jpeg">
+					<h2 class = "movie__title">Independence Day</h2>
+					<p class = "move__genre">Action</p>
+					<p class = "movie__time">120 min</p>
+				</div>
 
+				<div class = "movie l-movie">
+					<img class = "movie__image" src="The_Wolverine.jpeg">
+					<h2 class = "movie__title">The Wolverine</h2>
+					<p class = "move__genre">Action, Adventure</p>
+					<p class = "movie__time">126 min</p>
+				</div>
 
+				<div class="movie l-movie">
+					<img class = "movie__image" src="Independence_Day.jpeg">
+					<h2 class = "movie__title">Independence Day</h2>
+					<p class = "move__genre">Action</p>
+					<p class = "movie__time">120 min</p>
+				</div>
+
+				<div class="movie l-movie">
+					<img class = "movie__image" src="Independence_Day.jpeg">
+					<h2 class = "movie__title">Independence Day</h2>
+					<p class = "move__genre">Action</p>
+					<p class = "movie__time">120 min</p>
+				</div>
+
+				<div class="movie l-movie">
+					<img class = "movie__image" src="The_Wolverine.jpeg">
+					<h2 class = "movie__title">The Wolverine</h2>
+					<p class = "move__genre">Action, Adventure</p>
+					<p class = "movie__time">126 min</p>
+				</div>
+
+				<div class="movie l-movie">
+					<img class = "movie__image" src="Independence_Day.jpeg">
+					<h2 class = "movie__title">Independence Day</h2>
+					<p class = "move__genre">Action</p>
+					<p class = "movie__time">120 min</p>
+				</div>
+
+				<div class = "movie l-movie">
+					<img class = "movie__image" src="The_Wolverine.jpeg">
+					<h2 class = "movie__title">The Wolverine</h2>
+					<p class = "move__genre">Action, Adventure</p>
+					<p class = "movie__time">126 min</p>
+				</div>
+
+				<div class="movie l-movie">
+					<img class = "movie__image" src="Independence_Day.jpeg">
+					<h2 class = "movie__title">Independence Day</h2>
+					<p class = "move__genre">Action</p>
+					<p class = "movie__time">120 min</p>
+				</div>
+
+				<div class="movie l-movie">
+					<img class = "movie__image" src="Independence_Day.jpeg">
+					<h2 class = "movie__title">Independence Day</h2>
+					<p class = "move__genre">Action</p>
+					<p class = "movie__time">120 min</p>
+				</div>
+			</div>
+		</div>
+	<!-- Start of chat bar  -->
+
+		<div class="chat">
+
+			<div class = "chat__user">
+				<ul>
+					<li>Nels Findley</li>
+				</ul>
+		</div>
+
+			<div class = "chat__online">
+
+				<h2 class = "chat__title">Online</h2>
+				<ul>
+					<li>Victor Erixon</li>
+						<p class = "chat__watching">The Great Gatsby</p>
+					<li>Gustav Butlex</li>
+						<p class = "chat__watching">Wolverine</p>
+				</ul>
+
+				<h2 class = "chat__title">Offline</h2>
+
+				<ul>
+					<li>Bill S Kenney</li>
+						<p class = "chat__watching">Jobs</p>
+					<li>Charlie Waite</li>
+						<p class = "chat__watching">Pacific Rim</p>
+				</ul>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
